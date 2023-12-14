abeaefcd6be2bfe1c74f392065fdff6eef7871e4
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-14 17:18:25 +0000

    NLTVC added setup for large titles in nav bar

--- GoodNewsMVVM/Controllers/NewsListTableViewController.swift
+++ GoodNewsMVVM/Controllers/NewsListTableViewController.swift
@@ -9,7 +9,13 @@ import Foundation
 import UIKit
 
 class NewsListTableViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
