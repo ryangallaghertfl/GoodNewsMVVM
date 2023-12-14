5835d26b00126d4535daee5d1adf30b215447780
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-14 17:04:34 +0000

    removed test examples

---
+++ GoodNewsMVVM/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewsListTableViewController.swift
+//  GoodNewsMVVM
+//
+//  Created by Ryan Gallagher on 14/12/2023.
+//
+
+import Foundation
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+    
+}
