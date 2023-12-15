c6b83fe9370d5f1d21570853b8b890731242ee14
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-15 11:01:20 +0000

    Article model added

---
+++ GoodNewsMVVM/Models/Article.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  GoodNewsMVVM
+//
+//  Created by Ryan Gallagher on 15/12/2023.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    let title: String
+    let description: String
+}
