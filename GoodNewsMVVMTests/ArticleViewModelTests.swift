b87ba3ed656f921782b30b27da2289f9023d6629
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-15 16:40:55 +0000

    test: ArticleListViewModel_Init_shouldCorrectlyBePopulatedByArray

--- GoodNewsMVVMTests/ArticleViewModelTests.swift
+++ GoodNewsMVVMTests/ArticleViewModelTests.swift
@@ -28,5 +28,14 @@ final class ArticleViewModelTests: XCTestCase {
         XCTAssertEqual(viewModel.title, "Mock Title")
         XCTAssertEqual(viewModel.author, "Mock Author")
         }
+    
+    func test_ArticleListViewModel_Init_shouldCorrectlyBePopulatedByArray() {
+        let source1 = Source(id: "BBC", name: "MockName")
+        let source2 = Source(id: "ITV", name: "MockName2")
+            let articles = [Article(title: "Mock Title", author: "Mock Author", source: source1, publishedAt: "BBC", url: "BBC.com"), Article(title: "Fake Title", author: "Fake Author", source: source2, publishedAt: "ITV", url: "ITV.com")]
+            let listViewModel = ArticleListViewModel(articles: articles)
+            
+            XCTAssertEqual(listViewModel.numberOfRowsInSection(0), 2)
+        }
 
 }
