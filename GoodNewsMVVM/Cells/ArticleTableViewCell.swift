4792da5f62c66a12d46905104940cb729e767625
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-15 15:44:16 +0000

    added articleTVcell

---
+++ GoodNewsMVVM/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNewsMVVM
+//
+//  Created by Ryan Gallagher on 15/12/2023.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+}
