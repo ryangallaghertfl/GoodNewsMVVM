9ba062649129026bf644fb8659d72acccf7abb7b
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-15 14:01:38 +0000

    added articlelistVM

--- GoodNewsMVVM/ViewModels/ArticleViewModel.swift
+++ GoodNewsMVVM/ViewModels/ArticleViewModel.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
+struct ArticleListViewModel {
+    let articles: [Article]
+}
+
+extension ArticleListViewModel {
+    var numberOfSections: Int {
+        return 1
+    }
+    
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        return self.articles.count
+    }
+    
+    func articleAtIndex(_ index: Int) -> ArticleViewModel {
+        let article = self.articles[index]
+        return ArticleViewModel(article)
+    }
+}
+
 struct ArticleViewModel {
     private let article: Article
 }
